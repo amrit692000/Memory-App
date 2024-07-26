b7e2761df6e3c3f6eb923fb929005734787bdf83
amrit692000 <A00301070@mycambrian.ca>
2024-07-26 16:00:42 -0400

    Comment for "Add Memory" button  is addedAdd project files.

---
+++ HomePage.xaml.cs
@@ -0,0 +1,27 @@
+namespace Memory_App;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+
+
+    /*Event handler for when the "Add Memory" button is clicked.*/
+
+    /*<param name="sender">The object that triggered the event, typically the button that was clicked.*/
+    /*<param name="e">Event arguments related to the click event.</param>*/
+
+
+    /**This method is an asynchronous event handler that responds to the button click by navigating
+      to a new page called "AddMemory". The navigation operation is performed asynchronously to 
+    ensure that the user interface remains responsive during the transition.
+     */
+
+    private async void OnAddMemoryClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AddMemory());
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Memory_App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
